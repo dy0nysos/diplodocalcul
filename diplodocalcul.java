caf92b8303db1b8c8146ff5ff3fc2f43b90a055f
rabany.b@gmail.com <rabany.b@gmail.com@15bbb169-3e07-185a-d465-5c35e592cfa0>
2012-01-17 19:27:55 +0000

    -mise a jour de la gestion de la division, -optimisation de l'algorithme

--- diplodocalcul.java
+++ diplodocalcul.java
@@ -1,9 +1,27 @@
 class diplodocalcul extends Program{
+    //variables globales
     String operateurChoisi;
     double operande1=0,operande2=0;
+    //cette procedure genere aleatoirement en fonction du niveau un operateur qui sera utilise dans le calcul
     String [] reponse = new String [2];
-
-    // on genere un operateur en fonction du niveau
+    double resultatAddition(String nombre1,String nombre2){
+	double number1=Double.parseDouble(nombre1);
+	double number2=Double.parseDouble(nombre2);
+	println(number1+" + "+number2);
+	return (number1+number2);
+    }
+    double resultatSoustraction(String nombre1,String nombre2){
+	double number1=Double.parseDouble(nombre1);
+	double number2=Double.parseDouble(nombre2);
+	println(number1+" - "+number2);
+	return (number1-number2);
+    }
+    double resultatMultiplication(String nombre1,String nombre2){
+	double number1=Double.parseDouble(nombre1);
+	double number2=Double.parseDouble(nombre2);
+	println(number1+" x "+number2);
+	return (number1*number2);
+    }
     void operateur(int niveau){
 	String [] operateurs = new String [12];
 	operateurs[0]="+";//add simple = niveau 1
@@ -20,8 +38,8 @@ class diplodocalcul extends Program{
 	operateurs[11]="/";//division flottante = niveau 12
 	operateurChoisi=operateurs[(int)(random()*niveau)];
     }
-
-    //en fonction de l operateur genere on genere un calcul avec la fonction appropriee appelee dans l'algo principal
+    //En fonction de l'operateurChoisi, la procedure correspondante a l'operateur est appellee avec le niveau
+    //Elle genere deux nombres( int ou double selon le niveau) et les renvoie dans un tableau
     void addition(int niveau,String [] reponse){
 	operande1=0;
 	operande2=0;
@@ -31,9 +49,6 @@ class diplodocalcul extends Program{
 	    operande2=operande1;
 	    break;
 	case 2:
-	    operande1=(int)((random()*10)+1);
-	    operande2=(int)((random()*10)+1);
-	    break;
 	case 3:
 	    operande1=(int)((random()*10)+1);
 	    operande2=(int)((random()*10)+1);
@@ -51,28 +66,19 @@ class diplodocalcul extends Program{
 	    operande2=((int)((random()*1000)+1))/10.0;
 	    break;
 	case 7:
-	    operande1=((int)((random()*10000)+1))/100.0;
-	    operande2=((int)((random()*10000)+1))/100.0;    
+	    operande1=((int)((random()*1000)+1))/100.0;
+	    operande2=((int)((random()*1000)+1))/100.0;    
 	    break;
 	case 8:
-	    operande1=((int)((random()*100000)+1))/100.0;
-	    operande2=((int)((random()*100000)+1))/100.0;
+	    operande1=((int)((random()*10000)+1))/100.0;
+	    operande2=((int)((random()*10000)+1))/100.0;
 	    break;
 	case 9:
-	    operande1=((int)((random()*1000000)+1))/1000.0;
-	    operande2=((int)((random()*1000000)+1))/1000.0;
-	    break;
 	case 10:
-	    operande1=((int)((random()*1000000)+1))/10000.0;
-	    operande2=((int)((random()*1000000)+1))/10000.0;
-	    break;
 	case 11:
-	    operande1=((int)((random()*10000000)+1))/10000.0;
-	    operande2=((int)((random()*10000000)+1))/10000.0;
-	    break;
 	case 12:
-	    operande1=((int)((random()*10000000)+1))/10000.0;
-	    operande2=((int)((random()*10000000)+1))/10000.0;
+	    operande1=((int)((random()*10000)+1))/1000.0;
+	    operande2=((int)((random()*10000)+1))/10.0;
 	    break;
 	default:
 	    println("Un soucis a ete detecte");
@@ -81,22 +87,22 @@ class diplodocalcul extends Program{
 	reponse[0]=""+operande1;
 	reponse[1]=""+operande2;
     }
-    //fonction soustraction
+    //procedure de soustraction. Genere deux nombres ( int ou double selon le niveau) et les renvoie dans un tableau
     void soustraction(int niveau,String [] reponse){
 	operande1=0;
 	operande2=0;
 	double stock=0;
 	switch (niveau){
 	case 1:
-	   operande1=(int)(random()*10)+1;
-	   operande2=(int)(random()*10)+1;
+	    operande1=(int)(random()*10)+1;
+	    operande2=(int)(random()*10)+1;
 	    break;
 	case 2:
 	    operande1=(int)(random()*10)+1;
 	    operande2=(int)(random()*10)+1;
 	    break;
 	case 3:
-	    operande1=(int)(random()*50)+1;
+	    operande1=(int)(random()*25)+1;
 	    operande2=(int)(random()*10)+1;
 	    break;
 	case 4:
@@ -132,14 +138,14 @@ class diplodocalcul extends Program{
 	    operande2=((int)(random()*100000)+1)/1000.0;    
 	    break;
 	case 12:
-	    operande1=((int)(random()*1000000)+1)/1000.0;
-	    operande2=((int)(random()*1000000)+1)/100.0; 
+	    operande1=((int)(random()*100000)+1)/1000.0;
+	    operande2=((int)(random()*100000)+1)/1000.0; 
 	    break;
 	default:
 	    println("Un soucis a ete detecte");
 	    break;
 	}
-	//les primaire ne maitrisant pas les chiffres négatifs on doit vérifier que les résultats ne peuvent être négatif
+	//gestion du cas ou le resultat est negatif.
 	if (operande1-operande2<0){
 	    stock=operande1;
 	    operande1=operande2;
@@ -148,7 +154,7 @@ class diplodocalcul extends Program{
 	reponse[0]=""+operande1;
 	reponse[1]=""+operande2;
     }
-    //fonction multiplication
+    //procedure multiplication. Genere deux nombres (int ou double selon niveau) et renvoie dans un tableau
     void multiplication(int niveau,String [] reponse){
 	operande1=0;
 	operande2=0;
@@ -200,7 +206,7 @@ class diplodocalcul extends Program{
 	reponse[0]=""+operande1;
 	reponse[1]=""+operande2;
     }
-    //fonction division.
+    //Procedure division. Genere deux nombres (int ou double selon niveau) et les renvoie dans un tableau
     void division(int niveau,String [] reponse){
 	operande1=0;
 	operande2=0;
@@ -210,20 +216,20 @@ class diplodocalcul extends Program{
 	    operande2=(int)((random()*9)+1);
 	    break;
 	case 9:
-	    operande1=(int)((random()*99)+1);
+	    operande1=(int)((random()*19)+1);
 	    operande2=(int)((random()*9)+1);
 	    break;
 	case 10:
-	    operande1=(int)((random()*999)+1);
-	    operande2=(int)((random()*99)+1);
+	    operande1=(int)((random()*59)+1);
+	    operande2=(int)((random()*18)+1);
 	    break;
 	case 11:
-	    operande1=(int)(random()*9999)+1;
-	    operande2=(int)(random()*99)+1;
+	    operande1=(int)(random()*99)+1;
+	    operande2=(int)(random()*30)+1;
 	    break;
 	case 12:
-	    operande1=((int)(random()*10000)+1)/100.0;
-	    operande2=(int)((random()*99)+1);
+	    operande1=((int)(random()*1000)+1);
+	    operande2=(int)((random()*50)+1);
 	    break;
 	default:
 	    println("Un soucis");
@@ -231,16 +237,17 @@ class diplodocalcul extends Program{
 	}
 	reponse[0]=""+operande1;
 	reponse[1]=""+operande2;
+	println(reponse[0]+"    "+reponse[1]);
     }
 
 
 
-    /*Algorithme principal */
+    /*Corps de l'algorithme*/
     void algorithm(){
 	int niveau=0,mode=0,score=0,calculs=0,classe=0;
-	double reponseADonner=0,reste=0,resteEleve=0, reponseEleve=0;
+	double reponseADonner=0,reste=0,resteEleve=0, reponseEleve=0,number1=0,number2=0;
 	boolean continuerCalcul=false,niveauCorrect=false,nomEtPrenom=false,modeCorrect=false;
-	String prenom="",nom="";
+	String prenom="",nom="",reponseString="",resteString="";
 
 	//Enregistrement nom et prenom
 	while(!nomEtPrenom){
@@ -287,58 +294,58 @@ class diplodocalcul extends Program{
 		    modeCorrect = true;
 		}
 		else{
-		    println("Veuillez choisir le mode:\n1)Apprentissage(illimité, rentrez 123456789 pour arreter.)\n2)Examen");
+		    println("Veuillez choisir le mode:\n1)Apprentissage(illimité)\n2)Examen");
 		    mode=readInt();
 		}
 	    }
 
-	//mode illimite. On calcule tant que l'utilisateur ne rentre pas 123456789 comme reponse.
+	//MODE APPRENTISSAGE. stop pour quitter
 	if(mode==1){
 	    while(!continuerCalcul){
-		//on genere un calcul et on verifie le résultat tant que l'utilisateur ne veut pas arreter
+		//on genere un calcul et on verifie le resultat tant que l'utilisateur ne veut pas arreter. On a egalement besoin du nombre de calcul
 		operateur(niveau);
 		calculs++;
-		//on calcule le résultat de l'operation
+		//on calcule le resultat de l'operation. Utilisation de l'operateurChoisi.
 		if(operateurChoisi.equals("+")){
 		    addition(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"+"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])+Double.parseDouble(reponse[1]);
+		    reponseADonner=resultatAddition(reponse[0],reponse[1]);
 		}
 		else if(operateurChoisi.equals("-")){
 		    soustraction(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"-"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])-Double.parseDouble(reponse[1]);
+		    reponseADonner=resultatSoustraction(reponse[0],reponse[1]);
 		}
 		else if(operateurChoisi.equals("*")){
 		    multiplication(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"*"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])*Double.parseDouble(reponse[1]);
+		    reponseADonner=resultatMultiplication(reponse[0],reponse[1]);
 		}
 		else{
 		    division(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"/"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])/Double.parseDouble(reponse[1]);
-		    reste=Double.parseDouble(reponse[0])%Double.parseDouble(reponse[1]);
+		    number1=Double.parseDouble(reponse[0]);
+		    number2=Double.parseDouble(reponse[1]);
+		    println(number1+"/"+number2);
+		    reponseADonner=(number1/number2);
+		    reste=number1%number2;
 		}
-
-		//demande de la réponse à l'élève.
-		if(operateurChoisi.equals("/")){
+		//demande de la reponse de l'eleve.
+		if(operateurChoisi.equals("/") && niveau<11){
 		    println("Quel est le quotient?");
-		    reponseEleve= readDouble();
+		    reponseString= readString();
 		    println("quel est le reste?");
-		    resteEleve=readDouble();
+		    resteString=readString();
 		}
 		else {
 		    println("quel est la réponse?");
-		    reponseEleve=readDouble();
+		    reponseString=readString();
 		}
-		if(reponseEleve==123456789 || resteEleve==123456789){
+		//flag de sortie du mode illimite
+		if(reponseString.equals("stop") || resteString.equals("stop")){
 		    continuerCalcul=true;
 		}
 		//on verifie le resultat. Si c est une division, on verifie egalement le reste
 		else{
+		    reponseEleve=Double.parseDouble(reponseString);
 		    if(reponseEleve==reponseADonner){
-			if(operateurChoisi.equals("/")){
+			if(operateurChoisi.equals("/") && niveau<11){
 			    if(resteEleve==reste){
 				score++;
 			    }
@@ -358,41 +365,54 @@ class diplodocalcul extends Program{
 	    }	
 	    println("Vous avez réalisé un score de: "+score+" points sur "+(calculs-1));
 	}
-	//on boucle sur 10 calculs.
+	//GESTION DU MODE EXAMEN
+	//Boucle sur 10 calcul.
 	else if(mode==2){
 	    for(int i=0;i<=9;i++){
-		//on genere un calcul et on verifie le résultat tant que l'utilisateur ne veut pas arreter
+		//on genere un calcul et on verifie le resultat tant que l'utilisateur ne veut pas arreter
 		operateur(niveau);
 		calculs++;
-		//on calcule le résultat de l'operation
+		//on calcule le resultat de l'operation
 		if(operateurChoisi.equals("+")){
 		    addition(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"+"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])+Double.parseDouble(reponse[1]);
+		    reponseADonner=resultatAddition(reponse[0],reponse[1]);
 		}
 		else if(operateurChoisi.equals("-")){
 		    soustraction(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"-"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])-Double.parseDouble(reponse[1]);
+		    reponseADonner=resultatSoustraction(reponse[0],reponse[1]);
 		}
 		else if(operateurChoisi.equals("*")){
 		    multiplication(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"*"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])*Double.parseDouble(reponse[1]);
+		    reponseADonner=resultatMultiplication(reponse[0],reponse[1]);
 		}
 		else{
 		    division(niveau,reponse);
-		    println(Double.parseDouble(reponse[0])+"/"+Double.parseDouble(reponse[1]));
-		    reponseADonner=Double.parseDouble(reponse[0])/Double.parseDouble(reponse[1]);
-		    reste=Double.parseDouble(reponse[0])%Double.parseDouble(reponse[1]);
+		    number1=Double.parseDouble(reponse[0]);
+		    number2=Double.parseDouble(reponse[1]);
+		    println(number1+"/"+number2);
+		    reponseADonner=(number1/number2);
+		    reste=number1%number2;
 		}
-		reponseEleve= readInt();
-		if(reponseEleve==000){
+		//demande de la reponse de l'eleve.
+		if(operateurChoisi.equals("/") && niveau<11){
+		    println("Quel est le quotient?");
+		    reponseString= readString();
+		    println("quel est le reste?");
+		    resteString=readString();
+		}
+		else {
+		    println("quel est la réponse?");
+		    reponseString=readString();
+		}
+		//
+		if(reponseString.equals("stop") || resteString.equals("stop")){
 		    continuerCalcul=true;
 		}
+		//on verifie le resultat. Si c est une division, on verifie egalement le reste
 		else{
-		    if(reponseEleve==reponseADonner){
-			if(operateurChoisi.equals("/")){
+		    reponseEleve=Double.parseDouble(reponseString);
+		    if(reponseEleve<=reponseADonner+0.01 || reponseEleve >= reponseADonner-0.01){
+			if(operateurChoisi.equals("/") && niveau<11){
 			    if(resteEleve==reste){
 				score++;
 			    }
@@ -407,7 +427,7 @@ class diplodocalcul extends Program{
 		    else{
 			println("Réponse incorrecte.");
 			println("La réponse correcte était: "+reponseADonner+" ");
-		    }
+		    } 
 		}
 	    }
 	    println("Vous avez réalisé un score de: "+score+" points sur "+(calculs));
